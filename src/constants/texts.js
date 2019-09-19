5d435ee3bb44d0eb6f52c1a4bc76144cda8ef462
pcf26536 <muffwaindan@gmail.com>
2019-09-19 10:40:39 +0300

    [ch #168627421] add constants

---
+++ src/constants/texts.js
@@ -0,0 +1,19 @@
+const signUp = 'Sign Up';
+const contacts = 'Contacts';
+const findUs = 'Find Us On';
+const safaricom = '0798 884 419';
+const telkom = '0772 915 361';
+const sema = 'sema@rabbii.co.ke';
+const hr = 'hr@rabbii.co.ke';
+const menuTitle = 'Menu';
+const menuItems = ['Home', 'Our Services', 'About Us', 'FAQs', 'Contact Us', 'Trending'];
+const ourServices = ['TeeCha'];
+const hidden = 'hidden';
+
+
+const texts = {
+  menuTitle: menuTitle, menuItems: menuItems, ourServices: ourServices, hidden: hidden, signUp: signUp,
+  contacts: contacts, findUs: findUs, safaricom: safaricom, telkom: telkom, sema: sema, hr: hr
+};
+
+export default texts;
