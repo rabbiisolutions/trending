08882aa675aca52bf1f03b1417b6a83e7576186f
pcf26536 <muffwaindan@gmail.com>
2019-09-19 14:39:17 +0300

    [ft #168627435] rename images, add trending and blogs cards

---
+++ src/components/sub-items/BlogsCard.js
@@ -0,0 +1,7 @@
+import Container from "../utils/container";
+
+const BlogsCard = () => {
+
+};
+
+export default BlogsCard;
---
+++ src/components/sub-items/TrendingCard.js
@@ -0,0 +1,10 @@
+import Container from "../utils/container";
+import React from "react";
+
+const TrendingCard = () => {
+  return (
+    <Container />
+  );
+};
+
+export default TrendingCard;
