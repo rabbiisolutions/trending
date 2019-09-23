b365de86d1cd0767f1d50548be8ce376c6b9f46f
pcf26536 <muffwaindan@gmail.com>
2019-09-23 17:02:55 +0300

    [ft #168627435] add social icons float

---
+++ src/components/sub-items/SocialIcons.js
@@ -0,0 +1,17 @@
+import twitter from "../../icons/social/twitter-brands.svg"
+import youtube from "../../icons/social/youtube-brands.svg"
+import facebook from "../../icons/social/facebook-f-brands.svg"
+import wordpress from "../../icons/social/wordpress.svg"
+import Icon from "../utils/icon";
+import React from "react";
+
+const SocialIcons = () => {
+  return (<div className="icons">
+    <Icon src={youtube} height={2.5} width={2.5} className="youtube"/>
+    <Icon src={twitter} height={2.5} width={2.5} className="twitter"/>
+    <Icon src={facebook} height={2.5} width={2.5} className="facebook"/>
+    <Icon src={wordpress} height={2.5} width={2.5} className="twitter"/>
+  </div>);
+};
+
+export default SocialIcons;
