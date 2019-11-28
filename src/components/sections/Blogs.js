43fbe9dea8f3cf292c26144fe7bc7714e9bdd24c
pcf26536 <muffwaindan@gmail.com>
2019-11-28 10:13:25 +0300

    [ft #170011476] update blogs

--- src/components/sections/Blogs.js
+++ src/components/sections/Blogs.js
@@ -3,13 +3,6 @@ import ImageCard from "../sub-items/ImageCard";
 import texts from "../../constants/texts";
 import blogs from "../../constants/blogs";
 import MoreButton from "../sub-items/MoreButton";
-import webpIsSupported from "../../helpers/webp";
-import musicWebp from "../../images/webp/music.webp";
-import musicJpeg from "../../images/jpeg/music.jpg";
-import languageWebp from "../../images/webp/language.webp";
-import languageJpeg from "../../images/jpeg/language.jpg";
-import artWebp from "../../images/webp/art.webp";
-import artJpeg from "../../images/jpeg/art.jpg";
 import Overflow from "../utils/Overflow";
 
 const BlogsSection = () => {
@@ -17,16 +10,16 @@ const BlogsSection = () => {
       <section className="blogs">
         <div className="section-title">{texts.blogs}</div>
         <div className="images-frame">
-          <ImageCard number={1} title={blogs.music.title} src={webpIsSupported() ? musicWebp : musicJpeg}
-                     className="blog-image" alt="music-blog" content={blogs.music.text.substring(0, 160) + texts.more}
+          <ImageCard number={5} title={blogs.cooking.title} src={'https://rabbiits.files.wordpress.com/2019/10/jonathan-borba-5e0d3lfoc1w-unsplash-1.jpg'}
+                     className="blog-image" alt="music-blog" content={blogs.cooking.text.substring(0, 160) + texts.more}
                      url="https://rabbiits.wordpress.com/2019/10/23/get-the-best-out-of-home-tuition-for-cooking/"
           />
-          <ImageCard number={2} title={blogs.language.title} src={webpIsSupported() ? languageWebp : languageJpeg}
-                     className="blog-image" alt="language-blog" content={blogs.language.text.substring(0, 160) + texts.more}
+          <ImageCard number={6} title={blogs.friends.title} src={'https://rabbiits.files.wordpress.com/2019/07/1dbd14d499a9e14053420c55a486f846.jpg'}
+                     className="blog-image" alt="language-blog" content={blogs.friends.text.substring(0, 160) + texts.more}
                      url="https://rabbiits.wordpress.com/2019/07/26/plus-friends/"
           />
-          <ImageCard number={3} title={blogs.art.title} src={webpIsSupported() ? artWebp : artJpeg}
-                     className="blog-image" alt="art-blog" content={blogs.art.text.substring(0, 160) + texts.more}
+          <ImageCard number={4} title={blogs.mentor.title} src={'https://rabbiits.files.wordpress.com/2019/10/jcob-nasyr-cupkdzqvtlk-unsplash.jpg'}
+                     className="blog-image" alt="art-blog" content={blogs.mentor.text.substring(0, 160) + texts.more}
                      url="https://rabbiits.wordpress.com/2019/10/02/04-3-areas-to-get-a-counsellor-mentor-for-your-child/"
           />
           <MoreButton link="https://rabbiits.wordpress.com/author/rabbiitech/"/>
